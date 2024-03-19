8f5b0b39ae8d38fd501c027b48af81aaa5faaf0d
StijnvanVeen <vanveen02@gmail.com>
2024-03-19 11:24:25 +0100

    update class drankje

--- source code/SomerenModel/drankje.cs
+++ source code/SomerenModel/drankje.cs
@@ -1,9 +1,14 @@
 ﻿namespace SomerenModel {
-    class drankje {
+    public class Drankje 
+    {
         public int dranknummer { get; set; }
+
         public string dranknaam { get; set; }
+
         public int inkoop {  get; set; }
+
         public int voorraad { get; set; }
+
         public double btw {  get; set; }
     }
 }
