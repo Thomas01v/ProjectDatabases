086dfdf6f8c43f7b585f998a4089ae0dad9f1384
Bas Meskers <686340@student.inholland.nl>
2024-03-19 11:10:52 +0100

    drankje in het model toegevoegd

---
+++ source code/SomerenModel/drankje.cs
@@ -0,0 +1,9 @@
+﻿namespace SomerenModel {
+    class drankje {
+        public int dranknummer { get; set; }
+        public string dranknaam { get; set; }
+        public int inkoop {  get; set; }
+        public int voorraad { get; set; }
+        public double btw {  get; set; }
+    }
+}
