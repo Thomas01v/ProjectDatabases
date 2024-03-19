8540d12dc5cda66c5adc1c66f54030edca34cc10
Thomas01v <thomas.verhoeven@live.nl>
2024-03-19 15:24:49 +0100

    wat code geschreven :de:

--- source code/SomerenUI/SomerenUI.Designer.cs
+++ source code/SomerenUI/SomerenUI.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SomerenUI {
+﻿using System;
+
+namespace SomerenUI {
     partial class SomerenUI {
         /// <summary>
         ///  Required designer variable.
@@ -22,7 +24,8 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SomerenUI));
             menuStrip1 = new System.Windows.Forms.MenuStrip();
             dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -32,28 +35,34 @@
             lecturersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             activitiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             roomsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            revenueReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             pnlDashboard = new System.Windows.Forms.Panel();
             lblDashboard = new System.Windows.Forms.Label();
             pnlStudents = new System.Windows.Forms.Panel();
-            pnlTeachers = new System.Windows.Forms.Panel();
-            pnlRooms = new System.Windows.Forms.Panel();
-            pictureBox1 = new System.Windows.Forms.PictureBox();
             listViewStudents = new System.Windows.Forms.ListView();
-            listViewTeachers = new System.Windows.Forms.ListView();
-            listViewRooms = new System.Windows.Forms.ListView();
             label1 = new System.Windows.Forms.Label();
+            pnlTeachers = new System.Windows.Forms.Panel();
+            listViewTeachers = new System.Windows.Forms.ListView();
+            pnlRooms = new System.Windows.Forms.Panel();
+            listViewRooms = new System.Windows.Forms.ListView();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            pnlRevenueReport = new System.Windows.Forms.Panel();
+            firstDate = new System.Windows.Forms.DateTimePicker();
+            lastDate = new System.Windows.Forms.DateTimePicker();
+            amountOfSales = new System.Windows.Forms.Label();
             menuStrip1.SuspendLayout();
             pnlDashboard.SuspendLayout();
             pnlStudents.SuspendLayout();
             pnlTeachers.SuspendLayout();
             pnlRooms.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            pnlRevenueReport.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
             // 
             menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { dashboardToolStripMenuItem, studentsToolStripMenuItem, lecturersToolStripMenuItem, activitiesToolStripMenuItem, roomsToolStripMenuItem });
+            menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { dashboardToolStripMenuItem, studentsToolStripMenuItem, lecturersToolStripMenuItem, activitiesToolStripMenuItem, roomsToolStripMenuItem, revenueReportToolStripMenuItem });
             menuStrip1.Location = new System.Drawing.Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
@@ -109,6 +118,13 @@
             roomsToolStripMenuItem.Text = "Rooms";
             roomsToolStripMenuItem.Click += roomsToolStripMenuItem_Click;
             // 
+            // revenueReportToolStripMenuItem
+            // 
+            revenueReportToolStripMenuItem.Name = "revenueReportToolStripMenuItem";
+            revenueReportToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            revenueReportToolStripMenuItem.Text = "Revenue report";
+            revenueReportToolStripMenuItem.Click += revenueReportToolStripMenuItem_Click;
+            // 
             // pnlDashboard
             // 
             pnlDashboard.Controls.Add(lblDashboard);
@@ -130,22 +146,48 @@
             // pnlStudents
             // 
             pnlStudents.Controls.Add(listViewStudents);
-            pnlStudents.Controls.Add(label1);
             pnlStudents.Location = new System.Drawing.Point(14, 36);
             pnlStudents.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             pnlStudents.Name = "pnlStudents";
             pnlStudents.Size = new System.Drawing.Size(1072, 621);
             pnlStudents.TabIndex = 2;
             // 
+            // listViewStudents
+            // 
+            listViewStudents.Location = new System.Drawing.Point(18, 56);
+            listViewStudents.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            listViewStudents.Name = "listViewStudents";
+            listViewStudents.Size = new System.Drawing.Size(875, 408);
+            listViewStudents.TabIndex = 1;
+            listViewStudents.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(15, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(134, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Students";
+            // 
             // pnlTeachers
             // 
             pnlTeachers.Controls.Add(listViewTeachers);
-            pnlTeachers.Controls.Add(label1);
             pnlTeachers.Location = new System.Drawing.Point(14, 36);
             pnlTeachers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             pnlTeachers.Name = "pnlTeachers";
             pnlTeachers.Size = new System.Drawing.Size(1072, 621);
             pnlTeachers.TabIndex = 2;
+            // 
+            // listViewTeachers
+            // 
+            listViewTeachers.Location = new System.Drawing.Point(18, 56);
+            listViewTeachers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            listViewTeachers.Name = "listViewTeachers";
+            listViewTeachers.Size = new System.Drawing.Size(875, 408);
+            listViewTeachers.TabIndex = 1;
+            listViewTeachers.UseCompatibleStateImageBehavior = false;
             // 
             // pnlRooms
             // 
@@ -157,6 +199,15 @@
             pnlRooms.Size = new System.Drawing.Size(1072, 621);
             pnlRooms.TabIndex = 2;
             // 
+            // listViewRooms
+            // 
+            listViewRooms.Location = new System.Drawing.Point(18, 56);
+            listViewRooms.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            listViewRooms.Name = "listViewRooms";
+            listViewRooms.Size = new System.Drawing.Size(875, 408);
+            listViewRooms.TabIndex = 1;
+            listViewRooms.UseCompatibleStateImageBehavior = false;
+            // 
             // pictureBox1
             // 
             pictureBox1.Image = (System.Drawing.Image)resources.GetObject("pictureBox1.Image");
@@ -167,42 +218,45 @@
             pictureBox1.TabIndex = 2;
             pictureBox1.TabStop = false;
             // 
-            // listViewStudents
+            // pnlRevenueReport
             // 
-            listViewStudents.Location = new System.Drawing.Point(18, 56);
-            listViewStudents.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            listViewStudents.Name = "listViewStudents";
-            listViewStudents.Size = new System.Drawing.Size(875, 408);
-            listViewStudents.TabIndex = 1;
-            listViewStudents.UseCompatibleStateImageBehavior = false;
+            pnlRevenueReport.Controls.Add(amountOfSales);
+            pnlRevenueReport.Controls.Add(firstDate);
+            pnlRevenueReport.Controls.Add(lastDate);
+            pnlRevenueReport.Location = new System.Drawing.Point(14, 36);
+            pnlRevenueReport.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            pnlRevenueReport.Name = "pnlRevenueReport";
+            pnlRevenueReport.Size = new System.Drawing.Size(1072, 621);
+            pnlRevenueReport.TabIndex = 2;
             // 
-            // listViewTeachers
+            // firstDate
             // 
-            listViewTeachers.Location = new System.Drawing.Point(18, 56);
-            listViewTeachers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            listViewTeachers.Name = "listViewTeachers";
-            listViewTeachers.Size = new System.Drawing.Size(875, 408);
-            listViewTeachers.TabIndex = 1;
-            listViewTeachers.UseCompatibleStateImageBehavior = false;
+            firstDate.Location = new System.Drawing.Point(45, 67);
+            firstDate.MaxDate = new DateTime(2024, 12, 31, 0, 0, 0, 0);
+            firstDate.MinDate = new DateTime(2024, 1, 1, 0, 0, 0, 0);
+            firstDate.Name = "firstDate";
+            firstDate.Size = new System.Drawing.Size(250, 27);
+            firstDate.TabIndex = 2;
+            firstDate.ValueChanged += onFirstDateValueChange;
+            firstDate.MaxDate = DateTime.Today;
             // 
-            // listViewRooms
+            // lastDate
             // 
-            listViewRooms.Location = new System.Drawing.Point(18, 56);
-            listViewRooms.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            listViewRooms.Name = "listViewRooms";
-            listViewRooms.Size = new System.Drawing.Size(875, 408);
-            listViewRooms.TabIndex = 1;
-            listViewRooms.UseCompatibleStateImageBehavior = false;
+            lastDate.Location = new System.Drawing.Point(45, 124);
+            lastDate.MaxDate = new DateTime(2024, 12, 31, 0, 0, 0, 0);
+            lastDate.MinDate = new DateTime(2024, 1, 1, 0, 0, 0, 0);
+            lastDate.Name = "lastDate";
+            lastDate.Size = new System.Drawing.Size(250, 27);
+            lastDate.TabIndex = 2;
+            lastDate.MaxDate = DateTime.Today;
             // 
-            // label1
+            // amountOfSales
             // 
-            label1.AutoSize = true;
-            label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            label1.Location = new System.Drawing.Point(15, 9);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(134, 41);
-            label1.TabIndex = 0;
-            label1.Text = "Students";
+            amountOfSales.AutoSize = true;
+            amountOfSales.Location = new System.Drawing.Point(45, 154);
+            amountOfSales.Name = "amountOfSales";
+            amountOfSales.Size = new System.Drawing.Size(0, 20);
+            amountOfSales.TabIndex = 3;
             // 
             // SomerenUI
             // 
@@ -215,6 +269,7 @@
             Controls.Add(pnlTeachers);
             Controls.Add(pnlRooms);
             Controls.Add(pnlDashboard);
+            Controls.Add(pnlRevenueReport);
             MainMenuStrip = menuStrip1;
             Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             Name = "SomerenUI";
@@ -226,10 +281,10 @@
             pnlStudents.ResumeLayout(false);
             pnlStudents.PerformLayout();
             pnlTeachers.ResumeLayout(false);
-            pnlTeachers.PerformLayout();
             pnlRooms.ResumeLayout(false);
             pnlRooms.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            pnlRevenueReport.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -249,10 +304,16 @@
         private System.Windows.Forms.Panel pnlStudents;
         private System.Windows.Forms.Panel pnlTeachers;
         private System.Windows.Forms.Panel pnlRooms;
+        private System.Windows.Forms.Panel pnlRevenueReport;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ListView listViewStudents;
         private System.Windows.Forms.ListView listViewTeachers;
         private System.Windows.Forms.ListView listViewRooms;
+        private System.Windows.Forms.ListView listRevenueReport;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem revenueReportToolStripMenuItem;
+        private System.Windows.Forms.DateTimePicker firstDate;
+        private System.Windows.Forms.DateTimePicker lastDate;
+        private System.Windows.Forms.Label amountOfSales;
     }
 }
